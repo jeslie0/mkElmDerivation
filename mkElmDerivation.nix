8a35f16ea02c8a1bd339e9753e435499fc24f0bf
James Leslie <jamesleslie@posteo.net>
2023-03-06 07:28:49 +0000

    Add --optimize flag to the elm command

--- mkElmDerivation.nix
+++ mkElmDerivation.nix
@@ -63,7 +63,7 @@ stdenv.mkDerivation {
        mkdir -p $out/share/doc
        ${lib.concatStrings (map (module: ''
          echo "compiling ${elmfile module}"
-         elm make ${elmfile module} --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
+         elm make ${elmfile module} --optimize --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
          ${lib.optionalString outputJavaScript ''
           echo "minifying ${elmfile module}"
           uglifyjs $out/${module}.${extension} --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
