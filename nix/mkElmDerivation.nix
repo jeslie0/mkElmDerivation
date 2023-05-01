ffb962e48b74f4e5506ed79f688cd4fe5ed989a5
James Leslie <jamesleslie@posteo.net>
2023-05-01 17:31:07 +0100

    [BREAKING] Update and tidy the mkElmDerivation.nix derivation

--- nix/mkElmDerivation.nix
+++ nix/mkElmDerivation.nix
@@ -1,36 +1,88 @@
 { stdenv, elm, lib, uglify-js, snapshot, allPackagesJsonPath, elmHashesJsonPath }:
 {
-  # The name of the elm project.
-  pname
+  # This set is passed into a mkDerivation call. It needs a pname and
+  # version, or just a name.
 
-  # The version of the elm project.
-, version
 
   # The base directory of your elm project (most likely ./.).
-, src
+  src ? ./.
 
   # Optional: The elm project's elm.json file. Will default to
   # ${src}/elm.json
 , elmJson ? "${src}/elm.json"
 
-  # Optional: The directory containing the .elm files to
-  # compile. Currently, we only support using one directory.
-, srcdir ? with builtins; "${src}/${head ((fromJSON (readFile elmJson))."source-directories")}"
+  # Optional: List of elm files to build, relative to src. Defaults to
+  # "Main.elm" in the directories listed in the elm.json
+  # source-directories value.
+, targets ? with builtins;
+  map (path: "${src}/" + path + "/Main.elm") ((fromJSON (readFile elmJson))."source-directories")
 
   # Optional: The version of elm used. Read from elm.json file.
 , elmVersion ? with builtins; (fromJSON (readFile elmJson))."elm-version"
 
-  # Optional: The files in srcdir to compile.
-, targets ? [ "Main" ]
-
   # Optional: Should the outputted files be JavaScript or HTML?
 , outputJavaScript ? false
+
+  # Optional: Should the documentation JSON be generated?
+, docs ? false
+
+  # Optional: What level of optimization should be applied to the elm
+  # code?
+  # 0 - Level 0: No optimization,
+  # 1 - Level 1: Elm's own optimization only (elm make --optimize),
+  # 2 - Level 1 and uglifyjs's minification. This is the most
+  # optimized and is only  available for JavaScript code.
+, optimizationLevel ? 2
+
 , ...
 } @ args:
+let
+  # The output file extension.
+  extension = if outputJavaScript then "js" else "html";
+
+  # Command to make the docs directory
+  makeDocsDirectory = if docs then "mkdir -p $out/share/doc " else " ";
+
+  # Command to make the documentation JSON
+  docsCommand = basename: if docs then "--docs $out/share/doc/${basename}.json " else " ";
+
+  # elm's optimization flag
+  optimizeCommand = if optimizationLevel == 0 then " " else "--optimize ";
+
+  # The output field, added to the elm make command
+  outputCommand = dotElmFreeBaseName: "--output $out/${dotElmFreeBaseName}.${extension}";
+
+  # The elm make command with optimization added if requested.
+  elmMakeCommand = target:
+    let basename = builtins.baseNameOf target;
+        dotElmFreeBasename = lib.removeSuffix ".elm" basename;
+    in
+    ''
+      echo "compiling ${target}"
+      elm make ${target} \
+      ${outputCommand dotElmFreeBasename} \
+      ${optimizeCommand} \
+      ${docsCommand dotElmFreeBasename}
+
+      ${uglifyjsCommand dotElmFreeBasename}
+    '';
+
+  # Command to minimize the elm output further.
+  uglifyjsCommand = dotElmFreeBasename:
+    if optimizationLevel == 2 && outputJavaScript
+    then ''
+      echo "optimizing ${dotElmFreeBasename}.${extension}"
+
+      uglifyjs $out/${dotElmFreeBasename}.${extension} \
+      --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+      | uglifyjs --mangle --output $out/${dotElmFreeBasename}.min.${extension}
+
+      rm $out/${dotElmFreeBasename}.${extension}
+    ''
+    else " ";
+in
 
 stdenv.mkDerivation (args // {
-  inherit pname version src;
-
   buildInputs = [ elm ]
     ++ lib.optional outputJavaScript uglify-js;
 
@@ -40,22 +92,11 @@ stdenv.mkDerivation (args // {
     elmJson;
 
   installPhase =
-    let
-      elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
-      extension = if outputJavaScript then "js" else "html";
-    in
     ''
-         runHook preInstall
-         mkdir -p $out/share/doc
-         ${lib.concatStrings (map (module: ''
-           echo "compiling ${elmfile module}"
-           elm make ${elmfile module} --optimize --output $out/${module}.${extension} --docs $out/share/doc/${module}.json
-           ${lib.optionalString outputJavaScript ''
-            echo "minifying ${elmfile module}"
-            uglifyjs $out/${module}.${extension}  --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-            | uglifyjs --mangle --output $out/${module}.min.${extension}
-        ''}
-      '') targets)}
-          runHook postInstall
+    runHook preInstall
+    mkdir $out
+    ${makeDocsDirectory}
+    ${builtins.concatStringsSep "\n" (builtins.map elmMakeCommand targets)}
+    runHook postInstall
     '';
 })
